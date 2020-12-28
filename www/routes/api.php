50e33eb4be3a4769bda0a25942be1e314be3f101
lcshum <melcshum@gmail.com>
2020-12-28 15:44:17 +0800

    add api setting

--- www/routes/api.php
+++ www/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\RegisterController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('register', [RegisterController::class, 'register']);
+Route::post('login', [RegisterController::class, 'login']);
+
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
